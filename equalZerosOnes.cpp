369f79f3c503c944deb48ea52097db15fb1d78dd
kkrath <khitish.rath@outlook.com>
2020-05-26 16:57:51 +0530

    optimized solution by removing one array traversal and doing it in N time than 2N time and N space

--- equalZerosOnes.cpp
+++ equalZerosOnes.cpp
@@ -12,21 +12,17 @@ class BinArray{
            and second will be the sum so far at the same index*/
         int maxArrWithEqZeorsAndOnes(int arr[],int length){
             int maxLength = 0;
-            int sumArr[length];
-            sumArr[0] = arr[0];
+            int sum = 0;
             unordered_map<int,int> umap;
             /* initialize the map for 0th case */
-            umap[0] = 0;
-            for(int i = 1; i < length; i++){
-                if (arr[i] == 0)
-                    sumArr[i] = -1 + sumArr[i-1];
-                else
-                    sumArr[i] = arr[i] + sumArr[i-1];
-            }
+            umap[0] = -1;
             for(int i = 0; i < length; i++){
-                int sum = sumArr[i];
+                if (arr[i] == 0)
+                    sum += -1;
+                else
+                    sum += arr[i];
                 if(umap.find(sum)!=umap.end())
-                    maxLength = max(maxLength,i - umap[sum] + 1);
+                    maxLength = max(maxLength,i - umap[sum]);
                 else
                     umap[sum] = i;
             }
@@ -36,7 +32,7 @@ class BinArray{
 
 int main(){
    BinArray* binArr;
-   int arr[]{1,0,0,1,0,0,0};
+   int arr[]{0,0,1,0,0,0,1,1};
    int length = sizeof(arr)/sizeof(int);
    cout << "maximum subarray with equal zeros and ones: " << binArr->maxArrWithEqZeorsAndOnes(arr,length) << endl;
 }
