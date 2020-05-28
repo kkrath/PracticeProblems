92f58160226629d8097d43008e7736081a1e66b4
kkrath <khitish.rath@outlook.com>
2020-05-29 03:05:39 +0530

    today's work

--- findArraySumK.cpp
+++ findArraySumK.cpp
@@ -1,11 +1,34 @@
 /* find a subset/subsequence of an array that adds upto K */
 
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class Solution{
 public:
-	vector<int> findSubSet(vector<int> nums,int k){
-		
+	void findSubSet(vector<int> &nums,vector<int> subset, int curr,int target){
+		if(target == 0){
+			for(int i:subset)
+				cout << i << ",";
+			cout << endl;
+		}else if(target < 0)
+			return;
+		else if(curr < 0)
+			return;
+		else if(target < nums[curr])
+			findSubSet(nums, subset, curr - 1, target);
+		else{
+			findSubSet(nums, subset, curr - 1, target);
+			subset.push_back(nums[curr]);
+			findSubSet(nums, subset, curr - 1, target - nums[curr]);
+		}
+		return;
 	}
+};
+int main(){
+vector<int> nums{4,5,6,10};
+vector<int> subset;
+int target = 15;
+Solution* sol;
+sol->findSubSet(nums, subset, nums.size() - 1, target);
 }
